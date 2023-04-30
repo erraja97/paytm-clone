5323b1245925987a5871fbd8cc52474aa61fca6e
Raja Gupta <erraja97@gmail.com>
2023-04-30 15:48:38 +0530

    Files Added

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        paytmSkyBlue: "#00baf2",
+        paytmDarkBlue: "#0f4a8a",
+        paytmBlack: "#101010",
+      },
+    },
+  },
+  screens: {
+    'sm': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'md': '768px',
+    // => @media (min-width: 768px) { ... }
+
+    'lg': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    'xl': '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    '2xl': '1536px',
+    // => @media (min-width: 1536px) { ... }
+  },
+  plugins: [],
+};
